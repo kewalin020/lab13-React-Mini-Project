6ad9ad101adefae34e868cecc36d11e469ca4fd3
66160020 <66160020@go.buu.ac.th>
2025-03-31 15:41:59 +0700

    Add Loading.jsx

---
+++ book-collection-manager/src/components/Loading.jsx
@@ -0,0 +1,13 @@
+// src/components/Loading.jsx
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="loading-container">
+      <div className="loading-spinner"></div>
+      <p>กำลังโหลด...</p>
+    </div>
+  );
+}
+
+export default Loading;
